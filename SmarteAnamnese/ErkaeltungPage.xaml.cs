5036a2975611ff943e4267f6f3b82cec6426ff13
unknown <2111080@stud.hs-mannheim.de>
2024-08-28 17:24:23 +0200

    erkältung bis druck

--- SmarteAnamnese/ErkaeltungPage.xaml.cs
+++ SmarteAnamnese/ErkaeltungPage.xaml.cs
@@ -7,4 +7,83 @@ public partial class ErkaeltungPage : ContentPage
 		InitializeComponent();
         BindingContext = ServiceHelper.GetService<ErkaeltungPageViewModel>();
     }
+
+  
+
+    private void OnLungenvorerkrankungYesButtonClicked(object sender, EventArgs e)
+    {
+        LungenvorerkrankungYesButton.BackgroundColor = Colors.Green;
+        LungenvorerkrankungNoButton.BackgroundColor = Colors.LightGray;
+    }
+
+    private void OnLungenvorerkrankungNoButtonClicked(object sender, EventArgs e)
+    {
+        LungenvorerkrankungNoButton.BackgroundColor = Colors.Green;
+        LungenvorerkrankungYesButton.BackgroundColor = Colors.LightGray;
+    }
+
+    private void OnAntibiotikaYesButtonClicked(object sender, EventArgs e)
+    {
+        AntibiotikaYesButton.BackgroundColor = Colors.Green;
+        AntibiotikaNoButton.BackgroundColor = Colors.LightGray;
+    }
+
+    private void OnAntibiotikaNoButtonClicked(object sender, EventArgs e)
+    {
+        AntibiotikaNoButton.BackgroundColor = Colors.Green;
+        AntibiotikaYesButton.BackgroundColor = Colors.LightGray;
+    }
+
+    private void OnGrippeYesButtonClicked(object sender, EventArgs e)
+    {
+        GrippeYesButton.BackgroundColor = Colors.Green;
+        GrippeNoButton.BackgroundColor = Colors.LightGray;
+    }
+
+    private void OnGrippeNoButtonClicked(object sender, EventArgs e)
+    {
+        GrippeNoButton.BackgroundColor = Colors.Green;
+        GrippeYesButton.BackgroundColor = Colors.LightGray;
+    }
+
+    private void OnGrippeimpfungYesButtonClicked(object sender, EventArgs e)
+    {
+        GrippeimpfungYesButton.BackgroundColor = Colors.Green;
+        GrippeimpfungNoButton.BackgroundColor = Colors.LightGray;
+    }
+
+    private void OnGrippeimpfungNoButtonClicked(object sender, EventArgs e)
+    {
+        GrippeimpfungNoButton.BackgroundColor = Colors.Green;
+        GrippeimpfungYesButton.BackgroundColor = Colors.LightGray;
+    }
+
+    private void OnCoronaYesButtonClicked(object sender, EventArgs e)
+    {
+        CoronaYesButton.BackgroundColor = Colors.Green;
+        CoronaNoButton.BackgroundColor = Colors.LightGray;
+    }
+
+    private void OnCoronaNoButtonClicked(object sender, EventArgs e)
+    {
+        CoronaNoButton.BackgroundColor = Colors.Green;
+        CoronaYesButton.BackgroundColor = Colors.LightGray;
+    }
+
+
+
+    private void OnUebelkeitButtonClicked(object sender, EventArgs e)
+    {
+        UebelkeitButton.BackgroundColor = Colors.Green;
+    }
+
+    private void OnErbrechenButtonClicked(object sender, EventArgs e)
+    {
+        ErbrechenButton.BackgroundColor = Colors.Green;
+    }
+    private void OnDurchfallButtonClicked(object sender, EventArgs e)
+    {
+        DurchfallButton.BackgroundColor = Colors.Green;
+    }
+
 }
--- SmarteAnamnese/ErkaeltungPageViewModel.cs
+++ SmarteAnamnese/ErkaeltungPageViewModel.cs
@@ -19,12 +19,246 @@ namespace SmarteAnamnese
         }
 
         [ObservableProperty]
-        string beschwerdeZeitraum;
-       
+        private string beschwerdeZeitraum;
+
+        [ObservableProperty]
+        private bool raucher;
+
+        [ObservableProperty]
+        private bool ploetzlicherBeginn;
+
+        [ObservableProperty]
+        private bool fieber;
+
+        [ObservableProperty]
+        private string fieberHoehe;
+
+        [ObservableProperty]
+        private bool nachtschweiß;
+
+        [ObservableProperty]
+        private bool halsSchmerzen;
+
+        [ObservableProperty]
+        private bool schluckBeschwerden;
+
+        [ObservableProperty]
+        private bool verlegteNasenAtmung;
+
+        [ObservableProperty]
+        private bool druck;
+
+        [ObservableProperty]
+        private bool hautsymptome;
+
+        [ObservableProperty]
+        private Color ploetzlicherBeginnYesButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color ploetzlicherBeginnNoButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color fieberYesButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color fieberNoButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color nachtschweißYesButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color nachtschweißNoButtonColor = Colors.LightGray;
+
+
+        [ObservableProperty]
+        private Color halsSchmerzenYesButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color halsSchmerzenNoButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color schluckBeschwerdenYesButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color schluckBeschwerdenNoButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color verlegteNasenAtmungYesButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color verlegteNasenAtmungNoButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color druckYesButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color druckNoButtonColor = Colors.LightGray;
+
+
+
+        [ObservableProperty]
+        private Color smokerYesButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color smokerNoButtonColor = Colors.LightGray;
+
+
+        [ObservableProperty]
+        private Color hautSymptomeYesButtonColor = Colors.LightGray;
+
+        [ObservableProperty]
+        private Color hautSymptomeNoButtonColor = Colors.LightGray;
+
+        [RelayCommand]
+        public void PloetzlicherBeginnYes()
+        {
+            PloetzlicherBeginn = true;
+            PloetzlicherBeginnYesButtonColor = Colors.Green;
+            PloetzlicherBeginnNoButtonColor = Colors.LightGray;      
+        }
+
+        [RelayCommand]
+        public void PloetzlicherBeginnNo()
+        {
+            PloetzlicherBeginn = false;
+            PloetzlicherBeginnYesButtonColor = Colors.LightGray;
+            PloetzlicherBeginnNoButtonColor = Colors.Green;
+        }
+
+        [RelayCommand]
+        public void FieberYes()
+        {
+            Fieber = true;
+            FieberYesButtonColor = Colors.Green;
+            FieberNoButtonColor = Colors.LightGray;    
+        }
+
+        [RelayCommand]
+        public void FieberNo()
+        {
+            Fieber = false;
+            FieberYesButtonColor = Colors.LightGray;
+            FieberNoButtonColor = Colors.Green;
+        }
+
+        [RelayCommand]
+        public void NachtschweißYes()
+        {
+            Nachtschweiß = true;
+            NachtschweißYesButtonColor = Colors.Green;
+            NachtschweißNoButtonColor = Colors.LightGray;
+        }
+
+        [RelayCommand]
+        public void NachtschweißNo()
+        {
+            Nachtschweiß = false;
+            NachtschweißYesButtonColor = Colors.LightGray;
+            NachtschweißNoButtonColor = Colors.Green;
+        }
+
+        [RelayCommand]
+        public void HalsSchmerzenYes()
+        {
+            HalsSchmerzen = true;
+            HalsSchmerzenYesButtonColor = Colors.Green;
+            HalsSchmerzenNoButtonColor = Colors.LightGray;
+        }
+
+        [RelayCommand]
+        public void HalsSchmerzenNo()
+        {
+            HalsSchmerzen = false;
+            HalsSchmerzenYesButtonColor = Colors.LightGray;
+            HalsSchmerzenNoButtonColor = Colors.Green;
+        }
+
+        [RelayCommand]
+        public void SchluckBeschwerdenYes()
+        {
+            SchluckBeschwerden = true;
+            SchluckBeschwerdenYesButtonColor = Colors.Green;
+            SchluckBeschwerdenNoButtonColor = Colors.LightGray;
+        }
+
+        [RelayCommand]
+        public void SchluckBeschwerdenNo()
+        {
+            SchluckBeschwerden = false;
+            SchluckBeschwerdenYesButtonColor = Colors.LightGray;
+            SchluckBeschwerdenNoButtonColor = Colors.Green;
+        }
+
+        [RelayCommand]
+        public void VerlegteNasenAtmungYes()
+        {
+            VerlegteNasenAtmung = true;
+            VerlegteNasenAtmungYesButtonColor = Colors.Green;
+            VerlegteNasenAtmungNoButtonColor = Colors.LightGray;
+        }
+
+        [RelayCommand]
+        public void VerlegteNasenAtmungNo()
+        {
+            VerlegteNasenAtmung = false;
+            VerlegteNasenAtmungYesButtonColor = Colors.LightGray;
+            VerlegteNasenAtmungNoButtonColor = Colors.Green;
+        }
+
+        [RelayCommand]
+        public void DruckYes()
+        {
+            Druck = true;
+            DruckYesButtonColor = Colors.Green;
+            DruckNoButtonColor = Colors.LightGray;
+        }
+
+        [RelayCommand]
+        public void DruckNo()
+        {
+            Druck = false;
+            DruckYesButtonColor = Colors.LightGray;
+            DruckNoButtonColor = Colors.Green;
+        }
+
+        [RelayCommand]
+
+        public  void SmokerYes()
+        {
+            Raucher = true;
+            SmokerNoButtonColor = Colors.LightGray;
+            SmokerYesButtonColor = Colors.Green;
+        }
+        [RelayCommand]
+        public void SmokerNo()
+        {
+            Raucher = false;
+            SmokerYesButtonColor = Colors.LightGray;
+            SmokerNoButtonColor = Colors.Green;
+        }
+
+        [RelayCommand]
+        public void HautSymptomeYes()
+        {
+            Hautsymptome = true;
+            HautSymptomeYesButtonColor = Colors.Green;
+            HautSymptomeNoButtonColor = Colors.LightGray;
+        }
+
+        [RelayCommand]
+        public void HautSymptomeNo()
+        {
+            Hautsymptome = false;
+            HautSymptomeYesButtonColor = Colors.LightGray;
+            HautSymptomeNoButtonColor = Colors.Green;
+        }
+
+
         [RelayCommand]
         public async Task  SaveErkaeltung()
         {
             erkaeltung.BeschwerdeZeitraum = BeschwerdeZeitraum;
+            //erkaeltung.FieberHoehe = FieberHoehe;
             await Application.Current.MainPage.DisplayAlert("Titel", erkaeltung.BeschwerdeZeitraum, "OK");
         }
 
